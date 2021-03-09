7f498e355af90dd5649afb08ca20a79ef541cf04
dishit2141 <dishit2141@gmail.com>
2021-03-09 12:53:07 +0530

    first ex commit

---
+++ Assign 1/datatyp.java
@@ -0,0 +1,16 @@
+class datatyp
+{
+	public static void main(String []args)
+	{
+		boolean b=false;
+		char c='c';
+		int i=10;
+		float f=12;
+		double d=12.22;
+		System.out.println("value of boolean="+b);
+		System.out.println("value of char="+c);
+		System.out.println("value of int = "+i);
+		System.out.println("value of float="+f);
+		System.out.println("value of double="+d);
+	}
+}
